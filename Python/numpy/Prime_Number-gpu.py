35405730fab1a0b0622726d6a725bbc295a9fd1d
hika019 <37758939+hika019@users.noreply.github.com>
2018-09-17 15:43:08 +0900

    Add files via upload

---
+++ Python/numpy/Prime_Number-gpu.py
@@ -0,0 +1,52 @@
+import time
+import cupy as cp
+
+def PrimeN():
+    m = int(input())#キーボードから範囲指定
+
+    primeN = []
+
+    Number_list = cp.arange(2,m+1)
+    
+    print(Number_list)#数字のリスト
+    
+    for i in range(len(Number_list)):
+    
+        b = Number_list[0]#Number_lsitの最初の要素＆割る数
+        c = Number_list[(Number_list % b !=0)]#Number_list / bが0でないものをcとする（素数候補）
+        primeN.append(b) #ｂが素数
+        print(b)
+        Number_list = cp.asnumpy(c)
+        
+        if len(Number_list) == 0:#Number_listが空になったら終了
+            break
+    
+    print(primeN)
+
+
+start = time.time()
+
+PrimeN()
+
+elapsed_time = time.time() - start
+
+
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+
+
+'''
+試作
+prime = []
+
+a = np.arange(2,14)
+print(a)
+
+
+b = a[0]
+c = a[~(a % b == 0)]
+print(c)
+prime = b
+print(prime)
+a = c
+'''
