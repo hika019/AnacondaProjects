cf69ceb0480d205bd3a1e0c1ab6844c3bf29fb75
hika019 <ikazuti2001@gmail.com>
2018-10-19 19:15:46 +0900

    up date Prime_Number.py

--- Python/numpy/Prime_Number.py
+++ Python/numpy/Prime_Number.py
@@ -1,11 +1,12 @@
 import time
 import numpy as np
+from pprint import pprint
 
 m = int(input())#キーボードから範囲指定
 
 start = time.time()
 
-primeN = []
+primeN = np.empty((0,0),float)
 
 Number_list = np.arange(2,m+1)
 
@@ -15,7 +16,7 @@ for i in range(len(Number_list)):
 
     b = Number_list[0]#Number_lsitの最初の要素＆割る数
     c = Number_list[~(Number_list % b ==0)]#Number_list / bが0でないものをcとする（素数候補）
-    primeN.append(b) #ｂが素数
+    primeN = np.append(primeN ,np.array(b)) #ｂが素数
     #print(primeN)
     Number_list = c
     
@@ -28,7 +29,7 @@ elapsed_time = time.time() - start
 
 print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
-
+#meru_list = 2 ** primeN -1
 
 '''
 試作
