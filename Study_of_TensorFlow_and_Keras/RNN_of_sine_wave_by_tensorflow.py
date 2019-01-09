69095e7889ff35bf0effc14fe8169b9afaa47ed2
hika019 <ikazuti2001@gmail.com>
2019-01-10 01:04:18 +0900

    update file

--- Study_of_TensorFlow_and_Keras/RNN_of_sine_wave_by_tensorflow.py
+++ Study_of_TensorFlow_and_Keras/RNN_of_sine_wave_by_tensorflow.py
@@ -15,7 +15,26 @@ def toy_problem(T=100, ampl=0.05):
     noise = ampl * np.random.uniform(low=-1.2, high=1.2, size=len(x))
     return sin(x) + noise
 
+T = 100
 f = toy_problem()
 #print(sin())
-plt.plot(f)
-plt.show()
+#plt.plot(f)
+#plt.show()
+
+length_of_sequences = 2 *T #全時系列の長さ
+maxlen = 25 #一つの長さ
+
+data = []
+target = []
+
+for i in range(0, length_of_sequences - maxlen + 1):
+    data.append(f[i:i + maxlen])
+    target.append(f[i + maxlen])
+
+#print(data)
+#print(target)
+
+X = np.array(data).reshape(len(data), maxlen, 1)
+Y = np.array(target).reshape(len(target), 1)
+
+print(X,Y)
