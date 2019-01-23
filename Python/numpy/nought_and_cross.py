76d46386f2f3768f58f89e7a9bed194b4679c457
hika019 <ikazuti2001@gmail.com>
2019-01-23 23:09:44 +0900

    add new file

--- Python/numpy/nought_and_cross.py
+++ Python/numpy/nought_and_cross.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
+nought = 0
+cross = 1
+
+user = nought #初手
+
 X = 3
 Y = 3
 win = 3
 
 def bord():
+    global bord
     bord = np.zeros([Y, X])    
     bord[:,:] = np.nan
     
@@ -23,9 +29,33 @@ def put():
         XY = select_XY()
         x, y = int(XY[0]), int(XY[1])
         if x <= X-1 and y <= Y-1 and x >=0 and y >=0:
-            return x,y
+            return y,x
             break
         #print(X, Y)
         print('エラーもう一度')
+
+
+def nought_or_cross():
+    N_or_C = user
+    print(N_or_C)
+    for i in range(X*Y):
+        Nought_or_Cross = N_or_C % 2
+        change_bord(Nought_or_Cross)
+        N_or_C = N_or_C +1
+
+
+def change_bord(nought_or_cross):
+    y, x = put()
+    print(y,x)
+    bord[y, x] = nought_or_cross
+    print(bord)
     
-print(put())
+
+def start():
+    print('初手{}'.format(user))
+    print(bord())
+    #bord()
+    nought_or_cross()
+
+
+start()
