9b4be2fb85d4b57a92f5db6b51a0ac84c7422dfc
hika019 <ikazuti2001@gmail.com>
2019-02-25 23:28:47 +0900

    update file

--- Study_of_TensorFlow_and_Keras/temperature.py
+++ Study_of_TensorFlow_and_Keras/temperature.py
@@ -1,32 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 24 23:16:44 2019
-
-@author: hikar
-"""
-
 import csv
 import numpy as np
+import pandas as pd
 
 
 
 def data_read(file_name = 'data.csv'):
-    a =[]
-    with open(file_name, 'r') as f:
-        reader = csv.reader(f)
-        header = next(reader)  # ヘッダーを読み飛ばしたい時
-        for row in reader:
-            print(row)
-            a.append(row)
-        return a
-        
-
-def data_organize():
-    data = data_read()
-    datas = np.delete(data, 1, 0)
-    return datas
+    data = pd.read_csv(file_name)
+    
+    data.columns = ['day', 'average_temperature']
+    
+    return data
 
 
-#a = data_organize(file_name)
-#data_read()
-a = data_read()
+#print(data_read())
+    
+FEATURE_VALUE = ['average_temperature']
+DIMENSION = len(FEATURE_VALUE)
+
+dfWeather = data_read()
+
+#ソート
+dfWeatherTrain = dfWeather.ix['2017-02-23':'2018-02-22', FEATURE_VALUE]
+dfWeatherTrain = dfWeatherTrain.sort_index()
+dfWeatherTrain = dfWeatherTrain.dropna()#欠番排除
