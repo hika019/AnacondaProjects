defd863e1a272f3366d77b21106115c1b9fb79b5
hika019 <ikazuti2001@gmail.com>
2019-02-28 23:38:46 +0900

    add new file

---
+++ Study_of_TensorFlow_and_Keras/sin_wave_by_keras.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.layers.recurrent import LSTM
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sin(x, T=100):#T*2の長さ
+    return np.sin(2.0 * np.pi * x / T)
+
+# sin波にノイズを付与する
+def toy_problem(T=100, ampl=0.05):
+    x = np.arange(0, 2 * T + 1)
+    noise = ampl * np.random.uniform(low=-1.0, high=1.0, size=len(x))
+    return sin(x) + noise
+
+f = toy_problem()
+#plt.plot(f)
+#plt.show()
+
+
