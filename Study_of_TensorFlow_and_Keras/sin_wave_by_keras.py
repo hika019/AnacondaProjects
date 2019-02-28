5a85b408933804d26e2a182b901394fde5417cbe
hika019 <ikazuti2001@gmail.com>
2019-02-28 23:46:04 +0900

    update file

--- Study_of_TensorFlow_and_Keras/sin_wave_by_keras.py
+++ Study_of_TensorFlow_and_Keras/sin_wave_by_keras.py
@@ -1,3 +1,5 @@
+#https://qiita.com/sasayabaku/items/b7872a3b8acc7d6261bf
+
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
@@ -20,3 +22,47 @@ f = toy_problem()
 #plt.show()
 
 
+def make_dataset(low_data, n_prev=100):
+
+    data, target = [], []
+    maxlen = 25
+
+    for i in range(len(low_data)-maxlen):
+        data.append(low_data[i:i + maxlen])
+        target.append(low_data[i + maxlen])
+
+    re_data = np.array(data).reshape(len(data), maxlen, 1)
+    re_target = np.array(target).reshape(len(data), 1)
+
+    return re_data, re_target
+
+
+#g -> 学習データ，h -> 学習ラベル
+g, h = make_dataset(f)
+
+
+
+# モデル構築
+
+# 1つの学習データのStep数(今回は25)
+length_of_sequence = g.shape[1] 
+in_out_neurons = 1
+n_hidden = 300
+
+model = Sequential()
+model.add(LSTM(n_hidden, batch_input_shape=(None, length_of_sequence, in_out_neurons), return_sequences=False))
+model.add(Dense(in_out_neurons))
+model.add(Activation("linear"))
+optimizer = Adam(lr=0.001)
+model.compile(loss="mean_squared_error", optimizer=optimizer)
+
+
+
+
+# 予測
+predicted = model.predict(g)
+plt.figure()
+plt.plot(range(25,len(predicted)+25),predicted, color="r", label="row_data")
+plt.plot(range(0, len(f)), f, color="b", label="predict_data")
+plt.legend()
+plt.show()
