ba959ef5f2ff73c4a2944dc62a4a04014e3ee169
hika019 <ikazuti2001@gmail.com>
2019-03-11 13:40:06 +0900

    update file

--- Study_of_TensorFlow_and_Keras/sin_wave_by_keras.py
+++ Study_of_TensorFlow_and_Keras/sin_wave_by_keras.py
@@ -1,13 +1,17 @@
 #https://qiita.com/sasayabaku/items/b7872a3b8acc7d6261bf
-#同じ量のノードなら層が多いほうが早い（一層と二層の場合）
+#同じ量のノードなら層が多いほうが早いかもしれない（一層と二層の場合）
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import random
 
 def sin(x, T=100):#T*2の長さ
     return np.sin(2.0 * np.pi * x / T)
@@ -41,7 +45,12 @@ def make_dataset(low_data, n_prev=100):
 #g -> 学習データ，h -> 学習ラベル
 g, h = make_dataset(f)
 
-
+future_test = g[175].T
+#↑
+# 1つの学習データの時間の長さ -> 25 ↓
+time_length = future_test.shape[1]
+# 未来の予測データを保存していく変数
+future_result = np.empty((0))
 
 # モデル構築
 
@@ -49,12 +58,13 @@ g, h = make_dataset(f)
 length_of_sequence = g.shape[1] 
 
 in_out_neurons = 1
-n_hidden = 80 #隠れ層の数
+n_hidden = 480 #隠れ層のノード数
 
 model = Sequential()
 model.add(LSTM(n_hidden, batch_input_shape=(None, length_of_sequence, in_out_neurons), return_sequences=True))
 model.add(Dropout(0.15))
 model.add(LSTM(n_hidden, return_sequences=False))
+
 
 model.add(Dense(in_out_neurons))
 model.add(Activation("linear"))
@@ -71,10 +81,26 @@ model.fit(g, h,
           )
 
 
-# 予測
 predicted = model.predict(g)
+
+
+
+# 未来予想
+for step2 in range(400):
+
+    test_data = np.reshape(future_test, (1, time_length, 1))
+    batch_predict = model.predict(test_data)
+
+    future_test = np.delete(future_test, 0)
+    future_test = np.append(future_test, batch_predict)
+
+    future_result = np.append(future_result, batch_predict)
+
+
+# sin波をプロット
 plt.figure()
-plt.plot(range(25,len(predicted)+25),predicted, color="r", label="row_data")
-plt.plot(range(0, len(f)), f, color="b", label="predict_data")
+plt.plot(range(25,len(predicted)+25),predicted, color="r", label="predict")
+plt.plot(range(0, len(f)), f, color="b", label="row")
+plt.plot(range(0+len(f), len(future_result)+len(f)), future_result, color="g", label="future")
 plt.legend()
 plt.show()
