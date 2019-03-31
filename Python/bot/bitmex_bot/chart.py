bef626480b3166481797aed82818667bb2dbca96
hika019 <ikazuti2001@gmail.com>
2019-03-31 21:25:44 +0900

    update file

--- Python/bot/bitmex_bot/chart.py
+++ Python/bot/bitmex_bot/chart.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import calendar
 from pprint import pprint
 import csv
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,13 +14,16 @@ def interval():
     
     hoge = "1m"
     hogehoge = "5m"
+    hogehogehoge = "1h"
     
     if interval == hoge:
         return 1
     elif interval == hogehoge:
         return 5
+    elif interval == hogehogehoge:
+        return 60
 
-def chart_old():
+def charts_old():
     datas = np.empty((0,6), int)
     #ohlcvList = np.zeros((24*50, 0))
     now = datetime.utcnow()
@@ -39,18 +41,20 @@ def chart_old():
     print(all_len)
     #minute = 150
     
-    since = (unixTime - 60 * minute) * 1000
+    since = (unixTime - 60 * minute)
     
     #過去の取引情報(1分足)を取得
     limit = 750
     #ohlcvList = np.array(bitmex().fetch_ohlcv(market(), ashi(), since, limit))
     for i in range( int(-(-all_len // limit)) +1):
         print(i)
-        data = np.array(bitmex().fetch_ohlcv(market(), ashi(), since +(limit * i), limit))
+        print(since)
+        print('{}こ'.format(since +(limit*i*1000)))
+        data = np.array(bitmex().fetch_ohlcv(market(), ashi(), since +(limit * i * 1000), limit))
+        
         datas = np.vstack((datas, data))
         print(len(data))
         #print(data)
-    #print(datas)
     
     
     #print(unixTime)
@@ -60,14 +64,9 @@ def chart_old():
     return datas
 
 
-def write_old():
-    with open('chart_data.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file, lineterminator='\n')
-        writer.writerows(chart_old())
-    print('終了')
 
-
-def chart_new():
+def chart_old():
+    datas = np.empty((0,6), int)
     #ohlcvList = np.zeros((24*50, 0))
     now = datetime.utcnow()
  
@@ -75,49 +74,61 @@ def chart_new():
     unixTime = calendar.timegm(now.utctimetuple())
     
     #xx分前の時間(ms)を算出
-    minute = 60*2 #2時間分
+    minute = 60*24*ago() #1日*ago()
+    
+    
+    #データ取得量
+    all_len = 750 #1日/足（一日に何本のデータをとるか）＊日数
+    print(all_len)
     #minute = 150
-    since = (unixTime - 60 * minute) * 1000
     
-    #過去の取引情報(1分足)を取得
-    limit = 100
-    ohlcvList = np.array(bitmex().fetch_ohlcv(market(), ashi(), since, limit))
+    since = (unixTime-(60 * 21 *1000))
     print(unixTime)
-    ohlcvList_new = np.delete(ohlcvList, 1,0)
-    ohlcvList_new = np.delete(ohlcvList, 0,1)
-    return ohlcvList_new
+    print(since)
 
+    #過去の取引情報(1分足)を取得
+    limit = 750
+    #ohlcvList = np.array(bitmex().fetch_ohlcv(market(), ashi(), since, limit))
 
-def write_new():
-    with open('chart_data.csv','a') as csv_file:
-        writer = csv.writer(csv_file, lineterminator='\n')
-        writer.writerows(chart_new)
-
-def read_data():
-    data = np.loadtxt('chart_data_base.csv', delimiter =',',usecols=(2,3))
-    data = np.delete(data, 1,1)
+    data = np.array(bitmex().fetch_ohlcv(market(), ashi(), since, limit))
+        
+    datas = np.vstack((datas, data))
+    print(len(data))
+        #print(data)
     
+    
+    #print(unixTime)
+    #ohlcvList = np.delete(ohlcvList, 744,0)
+    #ohlcvList = np.delete(ohlcvList, 0,1)
+    #print(ohlcvList)
+    return datas
+
+
+
+def write_old():
     with open('chart_data.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, lineterminator='\n')
-        writer.writerows(data)
-    return data
+        writer.writerows(charts_old())
+    print('終了')
 
-
-#data = chart_old()
+#data = charts_old()
 #print(data)
 #print(len(data))
 
 
-
-
-
 #相場データ（過去）
-write_old()
+#write_old()
 print('#############################')
 #pprint(chart_old())
 
-'''
-data = (read_data())
+def load_csv(file_name = 'chart_data.csv'):#読み込み
+    csv_data = np.empty((0,1),float)
+    csv_data = np.loadtxt(file_name, delimiter=",", usecols=(1)) #列指定
+    print(csv_data)
+    return csv_data
+
+
+
+data = chart_old()
 plt.plot(data)
 plt.show()
-'''
