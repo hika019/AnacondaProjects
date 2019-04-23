ff22de0c50d5d0b0ee294f02c94f2b90cb16f468
hika019 <ikazuti2001@gmail.com>
2019-04-24 00:00:08 +0900

    add new file

---
+++ PyTorch-gpu/lstm_by_pytorch.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torch.optim import SGD
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sin(x, T=100):
+    return np.sin(2 * np.pi * x / T)
+
+def toy_problem(T=150, ample=0.2):
+    x = np.arange(0,2 * T +1)
+    noise = ample * np.random.uniform(low=-1.0, high=1.0, size=len(x))
+    return sin(x) + noise
+
+#x = np.arange(0, 100, 0.2)
+f = toy_problem()
+
+
+
+plt.plot(f)
+plt.show()
