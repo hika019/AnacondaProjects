446e63a7c9e801eb4ce45c02b2cbffcd50f04cb8
hika019 <ikazuti2001@gmail.com>
2019-05-07 09:28:51 +0900

    update file

--- Python/Mersenne_Prime_Number.py
+++ Python/Mersenne_Prime_Number.py
@@ -1,12 +1,44 @@
 from sympy import isprime
-import numpy as np
 import time
 import csv
+
+from multiprocessing import Pool
+from multiprocessing import Process
+import multiprocessing as multi
 #from tqdm import tqdm
+
 
 def write(i, Number):
     with open('Mersenne_Number.txt', 'a', newline="\n") as f:
         f.write('2**{}-1= {}\n\n'.format(i, Number))
+
+
+def Mersenne_N(n,m):
+    for i in range(n,m+1):
+        if isprime(i):
+            Mersenne_Number = 2 ** i -1
+            
+            s = 4
+            for n in range(2, i):
+                s = (s**2 - 2) % Mersenne_Number
+                if s == 0:
+                    print(Mersenne_Number)
+                    #write(i, Mersenne_Number)
+            
+            '''
+            if isprime(Mersenne_Number):
+                print(Mersenne_Number)
+                #return Mersenne_Number, i
+                '''
+
+
+
+def Mersenne_prime_number():
+    pool = Pool(3)
+    Mersenne_Number = pool.apply_async(Mersenne_N,range(n,m+1))
+    #M_N= Mersenne_Number#[0]
+    #print(M_N)
+    
 
 
 
@@ -17,8 +49,11 @@ m = int(input())
 
 start = time.time()
 
+pool = Pool(3)
+Mersenne_N(n,m)
 
 
+'''
 mersenne_N = (2 ** i -1 for i in range(n, m+1 ,2) if isprime(i))
 
 for i in mersenne_N:
@@ -26,18 +61,7 @@ for i in mersenne_N:
         print(i)
 '''
 
-
-for i in (range(n, m+1, 2)):
-    if isprime(i):
-        Mersenne_Number = 2 ** i -1
-        if isprime(Mersenne_Number):
-            print(Mersenne_Number)
-        #Mersenne_Prime_list = np.append(Mersenne_Number)
-        
-        #write(i, Mersenne_Number)
-'''
-
-
+#Mersenne_N(n,m)
 
 elapsed_time = time.time() - start
 
