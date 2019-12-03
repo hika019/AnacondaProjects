be15ab9cafe20025274c94a3a6ca430ef1bc27fe
hika019 <ikazuti2001@gmail.com>
2019-12-04 00:39:31 +0900

    update file

--- Python/for.py
+++ Python/for.py
@@ -4,16 +4,19 @@ Created on Mon May  6 13:56:32 2019
 
 @author: hikar
 """
-
+from sympy import isprime
 import time
 
 start = time.time()
 
 
-number = tuple(i for i in range(20000))()
+number = (i for i in range(209) if isprime(i))
 
 #for i in number:print(i)
-print(number)
+mersenne_N = (2 ** i -1 for i in number if isprime(i))
+
+for i in mersenne_N:print(i)
+
 '''
 for i in range(200000):
     print(i)
