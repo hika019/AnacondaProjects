f59c1a94be4d0de9839a12d5c987d42f6527114f
hika019 <ikazuti2001@gmail.com>
2019-12-09 13:37:28 +0900

    update file

--- Python/vernam_chipher.py
+++ Python/vernam_chipher.py
@@ -2,10 +2,45 @@
 import binascii
 import random
 
-#data = 'あいうえお'
-data = 'abcd'
 
-hogehoge = ord("ぢ")
+data = "私は日本人です。"
+
+
+def binary_data(data, byte = 3):
+    binary_str = "0b"
+    hex_str = ""
+    
+    for i in data:
+        binary_i = str(bin(ord(i))[2:])
+        print(i, len(binary_i), binary_i)
+        
+        while len(binary_i) < 8*byte:
+            binary_i = "0{}".format(binary_i)
+            hex_binary_i = hex(int(binary_i))
+        
+        binary_str += binary_i
+        hex_str += str(hex_binary_i)
+    #print(len(binary_str))
+    return binary_str
+
+
+def make_kye(m_data):
+    key = ""
+    n = len(m_data) -2
+    print(n)
+    while len(key) <= n:
+        key += str(random.randint(0,1))
+    print(key)
+
+
+
+print(binary_data(data))
+print(make_kye(binary_data(data)))
+
+
+
+#復号
+#chr(int('0b100010101101', 0))
 
 
 """
@@ -15,33 +50,24 @@ print(type(hoge))
 hoge = binascii.hexlify(hoge)
 print(hoge)
 print(type(hoge))
-
 m= hoge
 print(m)
-
 n = len(hoge)
-"""
 a = random.randrange(1,100)
 b = random.randrange(1,100)
 x = random.randrange(100) %10
 key = 0
-
 #print(hogehoge)
 n =(len(str(hogehoge)))
-
-
 while(len(str(key)) < n):
     key += x
     x = a*x +b
-
 c = hogehoge ^ key
-
-
 print("平文={}".format(hogehoge))
 print("暗号文={}, key={}".format(c,key))
-
 m_n = c ^ key
 print("平文={}, key={}".format(m_n, key))
 print("")
 print("m={}, key={}, c={}".format(bin(hogehoge), bin(key), bin(c)))
 print("m_n={}, key_n={}".format(n, len(str(key))))
+"""
